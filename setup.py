6922e164635d6e9b1e510bc2449ea59ab828d2d5
YangYang <50861502+yangyangMarine@users.noreply.github.com>
2025-03-12 12:56:32 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,27 @@
-from setuptools import setup
+from setuptools import setup, find_packages
+
+setup(
+    name='pamseek',  # The name of your package
+    version='0.1.0',  # Package version
+    description='A package for underwater PAM data analysis',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Yang Y',
+    author_email='proyangy@gmail.com',
+    url='https://github.com/yangyangMarine/pamseek', 
+    packages=find_packages(), 
+    install_requires=[ 
+        'numpy',
+        'matplotlib',
+        'scipy',
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.9', 
+)
 
 
 def read_requirements(file_path):
